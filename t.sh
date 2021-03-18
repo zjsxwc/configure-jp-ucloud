0b95b325340807d7f4588db120b827fc6c7eed50
Wang Chao <watcher.hangzhou@foxmail.com>
2021-03-18 09:10:20 +0800

    Update t.sh

--- t.sh
+++ t.sh
@@ -4,4 +4,4 @@ apt install -y git
 apt install -y python-pip
 pip install git+https://github.com/shadowsocks/shadowsocks.git@master
 
-ssserver -p 3389 -k mypasswd -m rc4-md5 --workers 10 --pid-file /tmp/ss2.pid --log-file /tmp/ss2.log --user nobody -v -d start
+ssserver -p 3389 -k mypasswd -m aes-256-ctr --workers 10 --pid-file /tmp/ssserverx.pid --log-file /tmp/ss2.log --user nobody -v -d start
